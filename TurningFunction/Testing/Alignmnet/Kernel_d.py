c8bf43bb27b78394851d6dd9fa330fe2579b9ce3
A-Y-Qi <73361563+A-Y-Qi@users.noreply.github.com>
2022-02-20 10:17:37 -0500

    Add files via upload

---
+++ TurningFunction/Testing/Alignmnet/Kernel_d.py
@@ -0,0 +1,20 @@
+from numpy import *
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from numpy import linalg as LA
+from scipy import special
+
+#range over 2pi
+def Kernel_d(N,m,d):
+    #define x and y
+    x = (2*pi/N)*arange(-N/2.0,N/2.0)
+    y = (2*pi/N)*arange(-N/2.0,N/2.0)
+    #define constant A
+    A=pi*m*(m*exp(-(d**2)/(m**2))+sqrt(pi)*d+sqrt(pi)*d*special.erf(d/m))
+    #Generate a 2*2 matrix
+    Kernel_d_func = zeros((N,N))
+    for i in range (0,N):
+        for j in range(0,N):
+            Kernel_d_func[i][j]=(1/A)*exp((-(sqrt(x[i]**2+y[j]**2)-d)**2)/(m**2))
+    return Kernel_d_func
+    
