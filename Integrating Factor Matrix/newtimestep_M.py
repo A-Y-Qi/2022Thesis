65060f50456367d2f23eba9f3666d52478014994
A-Y-Qi <73361563+A-Y-Qi@users.noreply.github.com>
2022-05-02 14:45:20 -0400

    Update newtimestep_M.py

--- Integrating Factor Matrix/newtimestep_M.py
+++ Integrating Factor Matrix/newtimestep_M.py
@@ -21,12 +21,16 @@ def newtimestep_matrix(u, Non_lin, gamma,dt,N):
     Non_lin_fft=fft_2D_3DM(Non_lin, N)
     u_new_fft=copy(u_fft)
     u_new=copy(u)
+    w_number_use=int(N/2)
+    w_number= arange(-w_number_use,w_number_use)
     for k in range (0,N):
         for l in range (0,N):
+            kk=w_number[k]
+            ll=w_number[l]
             u_hat_phi=u_fft[:,k,l]
             Non_lin_hat_phi=Non_lin_fft[:,k,l]
-            matrixA=diagonal_matrixA(N,k,l,gamma,dt)
-            matrixB=diagonal_matrixB(N,k,l,gamma,dt)
+            matrixA=diagonal_matrixA(N,kk,ll,gamma,dt)
+            matrixB=diagonal_matrixB(N,kk,ll,gamma,dt)
             u_new_phi=copy(u_hat_phi)
             #u_new_phi=matmul(matrixA, u_hat_phi)+matmul(matrixB, Non_lin_hat_phi)
             u_new_phi=matrixA*u_hat_phi+matrixB*Non_lin_hat_phi
