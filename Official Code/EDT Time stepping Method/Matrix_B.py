0c956078abac20047380666aea3d124d8d49626e
A-Y-Qi <73361563+A-Y-Qi@users.noreply.github.com>
2022-06-28 13:22:42 -0400

    Update Matrix_B.py

--- Official Code/EDT Time stepping Method/Matrix_B.py
+++ Official Code/EDT Time stepping Method/Matrix_B.py
@@ -1,12 +1,5 @@
 from numpy import *
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-from numpy import linalg as LA
-from scipy.linalg import *
-from compute_3D_FFT import *
-from compute_2D_FFT import *
-from compute_3D_IFFT import *
-from compute_2D_IFFT import *
+
 
 ##In this function, we generate a 2D coefficient matrix
 ## with known x and y (k and l)
@@ -14,17 +7,21 @@ from compute_2D_IFFT import *
 
 ##Make sure k and l are integers but not values
 def diagonal_matrixB(N,k,l,gamma,dt):
+    w_number_use=int(N/2)
+    #w_number= fft.fftshift(arange(-w_number_use,w_number_use)+1)
+    w_number= arange(-w_number_use,w_number_use)+1
+    w_number[-1]=0
+    kk=w_number[k]
+    ll=w_number[l]     
     phi=(2*pi/N)*arange(-N/2.0,N/2.0)
     identity_matrix=identity(N)
     result=zeros(N).astype(complex)
     for i in range (0,N):
         theta=phi[i]
-        M=-1j*gamma*(k*(cos(theta))+l*(sin(theta)))
-        const=1j*dt*exp(dt*M*0.5)*sinc(dt*0.5*M)
+        M=-1j*gamma*(ll*(cos(theta))+kk*(sin(theta)))
+        const=1j*dt*exp(dt*M*0.5)*sinc(dt*0.5*M/pi)##note sinc in python has sin(pi*x)/x
         result[i]=const
     return result
-                
-    
                 
     
     
